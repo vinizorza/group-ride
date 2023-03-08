496e1ebd26abce55fb127d2ca8cbdcbe9dc50d35
Vinicius Zorzanelli <vinizorza@hotmail.com>
2023-03-08 10:56:12 +0000

    1: Created initial setup

---
+++ src/main/java/com/vzor/grouprideuserservice/GroupRideUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vzor.grouprideuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupRideUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupRideUserServiceApplication.class, args);
+	}
+
+}
